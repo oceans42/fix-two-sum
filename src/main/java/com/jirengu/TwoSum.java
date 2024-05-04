f33071cb257b8d7f06a9a7492a1a6fa37aa3e0b7
hyq <958575100@qq.com>
2024-05-04 19:20:33 +0800

    Update TwoSum.java

--- src/main/java/com/jirengu/TwoSum.java
+++ src/main/java/com/jirengu/TwoSum.java
@@ -5,8 +5,7 @@ public class TwoSum {
         //请修复下面的代码使得能正确计算打印Sum: 214
         byte num1 = 104;
         byte num2 = 110;
-        byte sum;
-        sum = num1 + num2;
+        int sum = num1 + num2;
         System.out.print("Sum: " + sum);
     }
 }
